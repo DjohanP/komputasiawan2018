0d44226254062fadbdf763517e01a1c1361abbbe
djohanprabowo <djohan.prabowo1927@gmail.com>
2018-03-07 19:50:00 +0700

    NO 1,2,4

---
+++ tugas1/tugas.sh
@@ -0,0 +1,16 @@
+#1
+#useradd awan -p $(echo buayakecil | openssl passwd -1 -stdin) -d /home/awankecil -m
+#2
+sudo echo "nameserver 202.46.129.2" | sudo tee -a /etc/resolv.conf
+sudo apt-get -y update
+#wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && dpkg -i erlang-solutions_1.0_all.deb
+#apt-get install -y esl-erlang
+#apt-get install -y elixir
+#yes | mix local.hex
+#yes | mix archive.install https://github.com/phoenixframework/archives/raw/master/phx_new.ez
+#yes | mix phx.new /home/vagrant/hello2
+
+#3
+#4
+sudo apt-get install -y squid
+sudo apt-get install -y bind9
