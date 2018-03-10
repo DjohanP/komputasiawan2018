5152cb5d59921321431f8cbb8fbbc54b9a6d22cd
Rangga <ranggasenatama@gmail.com>
2018-03-10 11:50:36 +0700

    let me do it NGINX + composer

--- tugas1/tugas.sh
+++ tugas1/tugas.sh
@@ -20,6 +20,19 @@ sudo apt-get install mysql-server
 mysql_secure_installation
 systemctl status mysql.service
 mysqladmin -p -u root version #test MySQL
+#COMPOSER
+sudo apt-get install curl php-cli php-mbstring git unzip
+cd ~
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" #verified installer
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+composer #test composer
+#NGINGX
+sudo apt-get install nginx
+sudo ufw app list
+sudo ufw allow 'Nginx HTTP'
+sudo ufw status
+systemctl status nginx
 
 
 #4
