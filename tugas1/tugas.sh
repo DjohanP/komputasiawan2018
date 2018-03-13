35becf20499c1ab13175d5b72ed3a73a6aa6c51c
djohanprabowo <djohan.prabowo1927@gmail.com>
2018-03-14 05:37:38 +0700

    Go

--- tugas1/tugas.sh
+++ tugas1/tugas.sh
@@ -22,8 +22,10 @@ sudo apt-get install -y php7.2-cli
 sudo apt install -y php7.2-fpm php7.2-mysql
 sudo apt install -y php7.2-curl php7.2-gd php7.2-mbstring php7.2-xmlrpc php7.2-xml php7.2-zip
 sudo systemctl restart php7.2-fpm
-
-
+cd /var/www/web2
+composer update -vvv
+cp .env.example .env
+php artisan key:generate
 #nginx
 sudo apt-get install -y nginx
 sudo service nginx start
