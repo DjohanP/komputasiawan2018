ce2afe8775ac9a84e05fa862cb7ce3d1251388b7
djohanprabowo <djohan.prabowo1927@gmail.com>
2018-04-09 14:19:21 +0700

    Sesi Lab

---
+++ tugas2/loadbalancer/nginx.sh
@@ -0,0 +1,14 @@
+echo "nameserver 202.46.129.2" | sudo tee -a /etc/resolv.conf
+sudo /etc/init.d/apache2 stop
+sudo apt-get update
+sudo apt-get -y install python-software-properties software-properties-common
+sudo apt-add-repository -y ppa:ondrej/php
+sudo apt-get update
+sudo apt-get -y install php7.2
+sudo apt-get -y install php7.2-fpm php7.2-cgi
+
+sudo apt-get -y install nginx
+
+sudo nginx -t
+sudo service php7.2-fpm start
+sudo service nginx restart
